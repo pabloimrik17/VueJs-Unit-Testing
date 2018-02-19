a8a4102bec688834bb82b801e4f8a82c6073a80c
pfguerra <pabloimrik17@gmail.com>
2018-02-19 17:53:17 +0100

    Added List Component and it first spect test. Added vue router and two routes.

---
+++ src/components/List.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>My To Do List</h1>
+  <br/>
+    <ul>
+      <li v-for="item in listItems" :key="item">{{ item }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'list',
+  data() {
+    return {
+      listItems: ['buy food', 'play games', 'sleep'],
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
