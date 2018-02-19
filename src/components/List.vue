60def0a023c8aee1b4db1e0c6b09007c8b89cd94
pfguerra <pabloimrik17@gmail.com>
2018-02-19 17:59:58 +0100

    Work in progress (...)

--- src/components/List.vue
+++ src/components/List.vue
@@ -2,6 +2,8 @@
   <div>
     <h1>My To Do List</h1>
   <br/>
+    <input v-model="newItem">
+    <button @click="addItemToList">Add</button>
     <ul>
       <li v-for="item in listItems" :key="item">{{ item }}</li>
     </ul>
@@ -14,7 +16,14 @@ export default {
   data() {
     return {
       listItems: ['buy food', 'play games', 'sleep'],
+      newItem: '',
     };
+  },
+  methods: {
+    addItemToList() {
+      this.listItems.push(this.newItem);
+      this.newItem = '';
+    },
   },
 };
 </script>
--- test/unit/specs/List.spec.js
+++ test/unit/specs/List.spec.js
@@ -7,4 +7,11 @@ describe('List.vue', () => {
     const ListComponent = new Constructor().$mount();
     expect(ListComponent.$el.textContent).to.contain('play games');
   });
+
+  it('adds a new item to list on click', () => {
+    const Constructor = Vue.extend(List);
+    const ListComponent = new Constructor().$mount();
+
+    ListComponent.newItem = 'brush my teeth';
+  });
 });
